708c7753568c56d6b33e4d07dc6f6ed8d0bed8e3
Eric Wong <107148516+ewbyf@users.noreply.github.com>
2022-08-05 18:41:39 -0500

    Test to see if path functions correctly

--- src/js/start.js
+++ src/js/start.js
@@ -1,16 +1,16 @@
 import {data} from "./data.js";
 
 function resetLeft(leftIndex) {
-    document.getElementById("left-pfp").src = `../../../img/${leftIndex}.jpg`;
-    document.getElementById("left-bg").style.backgroundImage = `url("../../../img/${leftIndex}.jpg")`
+    document.getElementById("left-pfp").src = `/img/${leftIndex}.jpg`;
+    document.getElementById("left-bg").style.backgroundImage = `url("/img/${leftIndex}.jpg")`
     document.getElementById("left-name").innerText = data[leftIndex]["name"];
     document.getElementById("left-followers").innerText = data[leftIndex]["followers"].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     document.getElementById("left-followers").setAttribute("data-target", data[leftIndex]["followers"]);
 }
 
 export function resetRight(rightIndex) {
-    document.getElementById("right-pfp").src = `../../../img/${rightIndex}.jpg`;
-    document.getElementById("right-bg").style.backgroundImage = `url("../../../img/${rightIndex}.jpg")`
+    document.getElementById("right-pfp").src = `/img/${rightIndex}.jpg`;
+    document.getElementById("right-bg").style.backgroundImage = `url("/img/${rightIndex}.jpg")`
     document.getElementById("right-name").innerText = data[rightIndex]["name"];
     document.getElementById("right-followers").innerText = data[rightIndex]["followers"].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     document.getElementById("right-followers").setAttribute("data-target", data[rightIndex]["followers"]);
